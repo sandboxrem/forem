582d918001800720094a4d511f1d147ba93ab66c
Rustam Mamedov <rustam@enova.com>
2015-11-11 23:57:46 -0600

    modified association to be forem_user everywhere

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -3,7 +3,7 @@ require 'friendly_id'
 module Forem
   class Topic < ActiveRecord::Base
     include Forem::Concerns::Viewable
-    #include Forem::Concerns::NilUser
+    include Forem::Concerns::NilUser
     include Workflow
     include Forem::StateWorkflow
 
@@ -14,7 +14,7 @@ module Forem
 
 
     belongs_to :forum
-    belongs_to :user, :class_name => Forem.user_class.to_s
+    belongs_to :forem_user, :class_name => Forem.user_class.to_s, :foreign_key => :user_id
     has_many   :subscriptions
     has_many   :posts, -> { order("forem_posts.created_at ASC") }, :dependent => :destroy
     accepts_nested_attributes_for :posts
